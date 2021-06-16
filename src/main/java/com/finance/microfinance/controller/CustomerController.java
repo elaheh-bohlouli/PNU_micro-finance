2d889d618ea9a5f5d9d86fd07868cebf8a49fef4
elaheh <eli.bohluli@gmail.com>
2021-06-17 03:40:34 +0430

    Initial commit

---
+++ src/main/java/com/finance/microfinance/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package com.finance.microfinance.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class CustomerController {
+}
---
+++ src/main/java/com/finance/microfinance/controller/Index.java
@@ -0,0 +1,13 @@
+package com.finance.microfinance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Index {
+    @RequestMapping(name = "/" ,method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
